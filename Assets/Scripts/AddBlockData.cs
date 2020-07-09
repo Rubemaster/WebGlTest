6835b709f405cfe3c02d4189b1131666a94b66ac
Rubemaster <ruben.rick@outlook.com>
2020-07-09 21:59:53 +0200

    Create AddBlockData.cs

---
+++ Assets/Scripts/AddBlockData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBlockData : MonoBehaviour{
+  public GameObject blockDataPrefab;
+  public GameObject scrollContent;
+  public GameObject eventSystem;
+  public void addBlockData(string value){
+    GLOBAL.blockData.Add(new GLOBAL.BlockData(true,"telecom",value));
+    
+  }
+}
