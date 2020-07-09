0612f8cccb16f0aa19b8795082bcf9799c3299c2
Rubemaster <ruben.rick@outlook.com>
2020-07-09 21:45:56 +0200

    Create GLOBAL.cs

---
+++ Assets/Scripts/GLOBAL.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+  public class BlockData{
+    public bool active;
+    public string host;
+    public string value;
+    public BlockData(bool activeIn,string hostIn, string valueIn){
+      active=activeIn;
+      host=hostIn;
+      value=valueIn;
+    }
+  }
+  public static List<BlockData> blockData;
+  void Start(){
+    blockData=new List<BlockData>();
+  }
+}
