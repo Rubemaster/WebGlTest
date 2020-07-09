a0736e842f0e7a7c3bf2f8d83a8eb7ed44ab3d98
Rubemaster <ruben.rick@outlook.com>
2020-07-09 22:15:19 +0200

    Update GLOBAL.cs

--- Assets/Scripts/GLOBAL.cs
+++ Assets/Scripts/GLOBAL.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Global : MonoBehaviour
+public class GLOBAL : MonoBehaviour
 {
   public class BlockData{
     public bool active;
