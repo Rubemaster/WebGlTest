c9cf32d81f13d7d1c59478ae4e72b06a9f10905b
Rubemaster <ruben.rick@outlook.com>
2020-07-10 16:24:37 +0200

    Create BlockFieldCommand.cs

---
+++ Assets/Scripts/BlockFieldCommand.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockFieldCommand : MonoBehaviour{
+  public GameObject blockField;
+  public GameObject eventSystem;
+  public void registerBlock(){
+    AddBlockData addBlockData=evenetSystem.GetComponent(typeof(AddBlockData)) as AddBlockData;
+    InputField inputField=blockField.GetComponent(typeof(InputField)) as InputField;
+    addBlockData.addBlockData(inputField.text);
+  }
+}
