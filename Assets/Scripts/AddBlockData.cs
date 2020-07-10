5dcecb2031e0e5b7d21a4fd57f6a7bd1d767eebf
Rubemaster <ruben.rick@outlook.com>
2020-07-10 17:40:45 +0200

    Update AddBlockData.cs

--- Assets/Scripts/AddBlockData.cs
+++ Assets/Scripts/AddBlockData.cs
@@ -10,6 +10,17 @@ public class AddBlockData : MonoBehaviour{
     GLOBAL.blockData.Add(new GLOBAL.BlockData(true,"telecom",value,GLOBAL.id));
     GLOBAL.id++;
   }
+  private GLOBAL.BlockData getBlockDataById(int idIn){
+    for(int i=0;i<GLOBAL.blockData.Count;i++){
+      if(GLOBAL.blockData[i].id==idIn){
+        return return GLOBAL.blockData[i];
+      }
+    }
+    return new GLOBAL.BlockData(false,"N/A","Malfunction",-1);
+  }
   private void makeDisplay(float y, int addressId){
+    GameObject newDisplay=Instantiate(blockDataPrefab,scrollContent.transform);
+    DisplayCommand displayCommand=newDisplay.GetComponent(typeof(DisplayCommand)) as DisplayCommand;
+    
   }
 }
