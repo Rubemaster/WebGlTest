3995f5ff5aaf3232503192ed1a4289ddb336986d
Rubemaster <ruben.rick@outlook.com>
2020-07-10 17:28:13 +0200

    Update AddBlockData.cs

--- Assets/Scripts/AddBlockData.cs
+++ Assets/Scripts/AddBlockData.cs
@@ -7,7 +7,7 @@ public class AddBlockData : MonoBehaviour{
   public GameObject scrollContent;
   public GameObject eventSystem;
   public void addBlockData(string value){
-    GLOBAL.blockData.Add(new GLOBAL.BlockData(true,"telecom",value));
+    GLOBAL.blockData.Add(new GLOBAL.BlockData(true,"telecom",value,GLOBAL.id));
     GLOBAL.id++;
   }
   private void makeDisplay(float y, int addressId){
